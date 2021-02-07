5128312ccc4d542381560267b92f17b77ef0a937
alexgpitts <alexgpitts@gmail.com>
2021-02-06 17:04:08 -0800

    got mostly working other than background processes and signals

--- smallsh.c
+++ smallsh.c
@@ -5,213 +5,97 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include "smallsh.h"
 
 
 /*
 The following program forks a child process. The child process then replaces the program using execv to run "/bin/ls". The parent process waits for the child process to terminate.
 */
 int main(){
+  char new_dir[256];
   int words = 1;
-  char line[1028];
-  int flag, flag2, flag3, newOut, newIn, temp, result, childStatus;  
-
+  char line[2048];
+  int flag, flag2, flag3, newOut, newIn, temp, result;
+  int childStatus = 0;  
+  //struct Command command;
+  //struct Command *commandptr; 
   //loop for shell
   while(1){
-    char **array;//pointer to array
-    char *output;//pointer to path for redirecting output
-    char *input; //pointer to path for redirecting input
-    
-    //this while loop was inspired by 
+    setbuf(stdout, NULL); //recommended by someone from class. needed to clear my buffer to clean up my output
+    struct Command command;
     while(result < 1){
-      flag = 0;
-      flag2 = 0;
-      flag3 = 0;
-      words = 1;
       printf(": ");
-      result = scanf("%1028[^\n]",line);
+      result = scanf("%2047[^\n]",line);
       getchar();
       
       if(result > 0){
-        //for loop to walk to end of line and counting words
-        for(int i = 1; line[i]!='\0'; i++){
-          if(line[i]=='>'){
-            flag = i;
-            
-          }
-          if(line[i]=='<'){
-            flag2 = i;
-            
-          }
-          if(line[i]=='&'){
-            flag3 = i;
-            
-          }
-
-          else if(line[i]==' ' || line[i]=='\n' || line[i]=='\t'){
-            words++;
-          }
-        }
-
-
-        //printf("\n%d, %d, %d", flag, flag2, flag3);
-        //allocate array to store correct number of words
-        //printf("\n%d, %d, %d\n", flag, flag2, flag3);
-        if(flag == 0 && flag2 == 0 && flag3 == 0){
-          //allocate array to store correct number of words
-          array = malloc(words * sizeof(char*));
-          //printf("hit");
-
-          //fill array
-            array[0] = strtok (line," ");
-            for(int w = 1; w < words; w++){
-              array[w] = strtok (NULL," ");
-            }
-        }
-
-        //if only redirecting stdout
-        else if(flag !=0 && flag2==0 && flag3==0){
-          //printf("%d", words); 
-          array = malloc((words - 1) * sizeof(char*));
-          array[0] = strtok (line," ");
-          //printf("token arg %d: %s\n", 0, array[0]);
-          for(int w = 1; w < words - 2; w++){
-            array[w] = strtok (NULL," ");
-            //printf("token arg %d: %s\n", w, array[w]);
-          }
-          array[words-2] = NULL;
-          output = strtok (NULL," >");
-          //printf("output file: %s\n", output);
-          fflush(stdout);
-        }
-
-
-        //if only redirecting stdin
-        else if(flag ==0 && flag2!=0 && flag3==0){
-          array = malloc((words - 1) * sizeof(char*));
-          array[0] = strtok (line," ");
-          for(int w = 1; w < words - 2; w++){
-            array[w] = strtok (NULL," ");
-          }
-          array[words-2] = NULL;
-          input = strtok (NULL," <");
-          //printf("input: %s\n", input);
-          fflush(stdout);
-        }
-
-        //if redirecting both stdin and stdout
-        else if(flag !=0 && flag2!=0 && flag3==0){
-          array = malloc((words - (words - 2)) * sizeof(char*));
-          array[0] = strtok (line," ");
-          for(int w = 1; w < (words - (words - 1)); w++){
-            array[w] = strtok (NULL," ");
-          }
-          array[words-(words - 1)] = NULL;
-          if(flag < flag2){
-            output = strtok (NULL," <>");
-            input = strtok (NULL," <>");
-
-          }
-          else{
-            input = strtok (NULL," ><");
-            output = strtok (NULL," <>");
-          }
-          printf("arg1: %s\targ2: %s\n", array[0], array[1]);
-          printf("input: %s\toutput: %s\n", input, output);
-          fflush(stdout);
-        }
-
+        command = get_command(line);
       }
+  
     }
     result = 0; 
 
-    // Fork a new process
-    pid_t spawnPid = fork();
-
-    switch(spawnPid){
-      case -1:
-        perror("fork()\n");
-        exit(1);
-        break;
-      case 0:
-        // In the child process
-        //printf("CHILD(%d) running ls command\n", getpid());
-        
-
-        //if only redirecting stdout
-        if(flag !=0 && flag2==0 && flag3==0){ 
-          newOut = open (output, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-          if(newOut == -1){
-            perror("newOUt fopen()");
-            exit(1);
-          }
-          result = dup2(newOut, 1);
-          if(result == -1){
-            perror("newOUt dub2()");
-            exit(2);
-          }
-        }
-
-        //if only redirecting stdin
-        else if(flag ==0 && flag2!=0 && flag3==0){
-          newIn = open(input, O_RDONLY);
-          if(newIn == -1){
-            perror("newIn open()");
-            exit(1);
-          }
-          result = dup2(newIn, 0);
-          if(result == -1){
-            perror("newIn dub2()");
-            exit(2);
-          }
-          
-
-        }
-
-        //if redirecting both stdin and stdout
-        else if(flag !=0 && flag2!=0 && flag3==0){
-
-          //redirecting stdout
-          newOut = open (output, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-          if(newOut == -1){
-            perror("newOUt fopen()");
-            exit(1);
-          }
-          result = dup2(newOut, 1);
-          if(result == -1){
-            perror("newOUt dub2()");
-            exit(2);
-          }
-
-          //redirecting stdin
-          newIn = open(input, O_RDONLY);
-          if(newIn == -1){
-            perror("newIn open()");
-            exit(1);
-          }
-          result = dup2(newIn, 0);
-          if(result == -1){
-            perror("newIn dub2()");
-            exit(2);
-          }
-        }
-
-        // Replace the current program with user entered command line
-        execvp(array[0], array);
-        // exec only returns if there is an error
-        perror("execve");
-        exit(2);
-        break;
-
-      default:
-        
-        // In the parent process
-        // Wait for child's termination
-        spawnPid = waitpid(spawnPid, &childStatus, 0);
-        
-        //printf("PARENT(%d): child(%d) terminated. Exiting\n", getpid(), spawnPid);
-        break;
+    // if user enters exit, exit shell
+    if(strcmp(command.array[0], "exit") == 0){
+      exit(0);
     }
-    free(array);
+    else if(strcmp(command.array[0], "cd")==0){
+      if(command.array[1] == NULL){
+        chdir(getenv("HOME"));
+      }
+      else{
+        chdir(command.array[1]);
+      }
+      
+    }
+  
+    else if(strcmp(command.array[0], "status") == 0){
+    
+      if(childStatus == 0){
+        printf("%d\n", 0);
+      }
+      else{
+        printf("%d\n", WIFEXITED(childStatus));
+      }
+     
+      
+    }
+    
+    // Fork a new process
+    else{
+      pid_t spawnPid = fork();
+
+      switch(spawnPid){
+        case -1:
+          perror("fork()\n");
+          exit(1);
+          break;
+        case 0:
+          // In the child process
+          //printf("CHILD(%d) running ls command\n", getpid());
+
+          //controls all the logic for a given command depending on special characters entered by user
+          child_logic(command); 
+
+          // Replace the current program with user entered command line
+          execvp(command.array[0], command.array);
+          
+          // exec only returns if there is an error
+          perror("execve");
+          exit(2);
+          break;
+
+        default:
+          
+          // In the parent process
+          // Wait for child's termination
+          spawnPid = waitpid(spawnPid, &childStatus, 0);
+          
+          
+          //printf("PARENT(%d): child(%d) terminated. Exiting\n", getpid(), spawnPid);
+          break;
+      }
+    }
     
   } 
   
---
+++ smallsh.h
@@ -0,0 +1,181 @@
+
+
+struct Command{
+    char **array;
+    int flag;
+    int flag2; 
+    int background; 
+    int comment; 
+    int newOut, newIn;
+    int words; 
+    char *output;//pointer to path for redirecting output
+    char *input; //pointer to path for redirecting input
+};
+
+struct Command get_command(char line[2048]){
+    struct Command command;
+    command.flag = 0;
+    command.flag2 = 0;
+    command.comment = 0;
+    command.words = 1;
+    for(int i = 1; line[i]!='\0'; i++){
+      if(line[i]=='>'){
+        command.flag = i;
+        
+      }
+      if(line[i]=='<'){
+        command.flag2 = i;
+        
+      }
+
+      else if(line[i]==' ' || line[i]=='\n' || line[i]=='\t'){
+        command.words++;
+      }
+    }
+
+    //printf("\n%d, %d, %d", flag, flag2, flag3);
+    //allocate array to store correct number of words
+    //printf("\n%d, %d, %d\n", flag, flag2, flag3);
+    if(command.flag == 0 && command.flag2 == 0){
+      //allocate array to store correct number of words
+      command.array = malloc((command.words +1) * sizeof(char*));
+      //printf("hit");
+
+      //fill array
+        command.array[0] = strtok (line," ");
+        for(int w = 1; w < command.words; w++){
+          command.array[w] = strtok (NULL," ");
+        }
+        command.array[command.words] = NULL;
+        // for(int i = 0; i<command.words; i++){
+        //   printf("%s\n", command.array[i]);
+        // }
+    }
+
+    //if only redirecting stdout
+    else if(command.flag !=0 && command.flag2==0){
+      //printf("%d", words); 
+      command.array = malloc((command.words - 1) * sizeof(char*));
+      command.array[0] = strtok (line," ");
+      //printf("token arg %d: %s\n", 0, array[0]);
+      for(int w = 1; w < command.words - 2; w++){
+        command.array[w] = strtok (NULL," ");
+        //printf("token arg %d: %s\n", w, array[w]);
+      }
+      command.array[command.words-2] = NULL;
+      command.output = strtok (NULL," >");
+      //printf("output file: %s\n", output);
+      //fflush(stdout);
+    }
+
+
+    //if only redirecting stdin
+    else if(command.flag ==0 && command.flag2!=0){
+      command.array = malloc((command.words - 1) * sizeof(char*));
+      command.array[0] = strtok (line," ");
+      for(int w = 1; w < command.words - 2; w++){
+        command.array[w] = strtok (NULL," ");
+      }
+      command.array[command.words-2] = NULL;
+      command.input = strtok (NULL," <");
+      //printf("input: %s\n", input);
+      //fflush(stdout);
+    }
+
+    //if redirecting both stdin and stdout
+    else if(command.flag !=0 && command.flag2!=0){
+      command.array = malloc((command.words - (command.words - 2)) * sizeof(char*));
+      command.array[0] = strtok (line," ");
+      for(int w = 1; w < (command.words - (command.words - 1)); w++){
+        command.array[w] = strtok (NULL," ");
+      }
+      command.array[command.words-(command.words - 1)] = NULL;
+      if(command.flag < command.flag2){
+        command.output = strtok (NULL," <>");
+        command.input = strtok (NULL," <>");
+
+      }
+      else{
+        command.input = strtok (NULL," ><");
+        command.output = strtok (NULL," <>");
+      }
+      //printf("arg1: %s\targ2: %s\n", array[0], array[1]);
+      //printf("input: %s\toutput: %s\n", input, output);
+      //fflush(stdout);
+    }
+    return command;
+}
+
+void child_logic(struct Command command){
+  int newOut, newIn, result;
+  // In the child process
+  //printf("CHILD(%d) running ls command\n", getpid());
+  
+  //if its a comment
+  if(command.array[0][0]=='#'){
+    exit(0);
+  }
+
+
+  //if only redirecting stdout
+  else if(command.flag !=0 && command.flag2==0){ 
+    newOut = open (command.output, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    if(newOut == -1){
+      perror("newOUt fopen()");
+      exit(1);
+    }
+    result = dup2(newOut, 1);
+    if(result == -1){
+      perror("newOUt dub2()");
+      exit(2);
+    }
+  }
+
+  //if only redirecting stdin
+  else if(command.flag ==0 && command.flag2!=0){
+    newIn = open(command.input, O_RDONLY);
+    if(newIn == -1){
+      perror("newIn open()");
+      exit(1);
+    }
+    result = dup2(newIn, 0);
+    if(result == -1){
+      perror("newIn dub2()");
+      exit(2);
+    }
+      
+
+  }
+
+  //if redirecting both stdin and stdout
+  else if(command.flag !=0 && command.flag2!=0){
+
+    //redirecting stdout
+    newOut = open (command.output, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    if(newOut == -1){
+      perror("newOUt fopen()");
+      exit(1);
+    }
+    result = dup2(newOut, 1);
+    if(result == -1){
+      perror("newOUt dub2()");
+      exit(2);
+    }
+
+    //redirecting stdin
+    newIn = open(command.input, O_RDONLY);
+      if(newIn == -1){
+      perror("newIn open()");
+      exit(1);
+    }
+    result = dup2(newIn, 0);
+      if(result == -1){
+      perror("newIn dub2()");
+      exit(2);
+    }
+        
+  }
+
+
+
+}
