4b7cbead88172c82985a8e644c26f4e5365e9822
alexgpitts <alexgpitts@gmail.com>
2021-02-08 12:31:53 -0800

    finished
    added comments and fixed an issue that was deleting inline ampesands in echo commands for example. Also deleted all unessisary files

--- smallsh.c
+++ smallsh.c
@@ -14,18 +14,20 @@ int killed =0; // to see if chiled was ctrl+c ended
 
 
 void catchSIGTSTP(int signo){
-  //print message
+  //if we are in forground only mode when signal is caught, then print message and exit forground only mode with "mode" flag
   if(mode == 1){
     char* message = "\nExiting foreground-only mode\n";
     write(STDOUT_FILENO, message, 30);
     mode = 0;
   }
+
+  //if we are not in forground only mode when signal is caught, then print message and enter forground only mode with "mode" flag
   else if(mode == 0){
     char* message = "\nEntering foreground-only mode (& is now ignored)\n";
     write(STDOUT_FILENO, message, 50);
     mode = 1;
   }
-  
+  //reprint prompt because when I caught signals it wasnt redisplaying it.
   char* message3 = ":";
   write(STDOUT_FILENO, message3, 1);
   
@@ -37,52 +39,55 @@ void catchSIGTSTP(int signo){
 The following program forks a child process. The child process then replaces the program using execv to run "/bin/ls". The parent process waits for the child process to terminate.
 */
 int main(){
-  struct sigaction SIGINT_action = {0}, SIGTSTP_action = {0}; 
-  SIGINT_action.sa_handler = SIG_IGN; 
-  SIGINT_action.sa_flags = SA_RESTART;
-  sigfillset(&SIGINT_action.sa_mask);
-  sigaction(SIGINT, &SIGINT_action, NULL);
+  struct sigaction SIGINT_action = {0}, SIGTSTP_action = {0}; //signal handler structs for ctr+c and ctr+z 
+  SIGINT_action.sa_handler = SIG_IGN; //set ctr+c to ignore
+  SIGINT_action.sa_flags = SA_RESTART; //set flag to restart after catch (dont know if I need this but I think lecture said it was good practice)
+  sigfillset(&SIGINT_action.sa_mask); 
+  sigaction(SIGINT, &SIGINT_action, NULL); //set action on SIGINT to behave like SIGINT action
 
-  SIGTSTP_action.sa_handler = catchSIGTSTP;
-  SIGTSTP_action.sa_flags = SA_RESTART;
+  SIGTSTP_action.sa_handler = catchSIGTSTP; //set ctr+z to signal handler at top of this file
+  SIGTSTP_action.sa_flags = SA_RESTART; //set flag to restart after each catch
   sigfillset(&SIGTSTP_action.sa_mask);
-  sigaction(SIGTSTP, &SIGTSTP_action, NULL);
+  sigaction(SIGTSTP, &SIGTSTP_action, NULL); //set sigaction for ctr+z to behave like SIGTSTP_action
 
 
-  char new_dir[256];
-  int words = 1;
-  char line1[2048] ="";
-  char line[3000] = "";
-  int flag, flag2, flag3, newOut, newIn, temp, result;
-  int childStatus = 0;  
-  int length = snprintf( NULL, 0, "%d", getpid());
+  // char new_dir[256]; 
+  int words = 1; //counter for number of words in my command line minus any redirections 
+  char line1[2048] =""; //array for inital read in value
+  char line[3000] = ""; //array that gets everything from "line1", but handles variabel expansion
+  // int flag, flag2, flag3, newOut, newIn, temp, result; //initalizing all my ints 
+  int result; //to hold return value from waitpid() in the parent part of my switch statement
+  int childStatus = 0; //to hold child status. Set to zero initally for if no child gets ran and we call "status"
+
+  //calculating length of PID and storing it in char ID[].
+  int length = snprintf( NULL, 0, "%d", getpid()); 
   char* ID = malloc( length + 1 );
   snprintf( ID, length + 1, "%d", getpid());
+
+
+  //counters
   int k = 0;
   int j =0; 
   int i =0;
   int z= 0;
 
+  //initialize an array of background PIDs. 
   int processes[100];
   for(i = 0; i<100; i++){
     processes[i] = 0; 
   }
   i = 0; 
   
-
-  //struct Command command;
-  //struct Command *commandptr; 
   //loop for shell
   while(1){
-    
   
-    
     setbuf(stdout, NULL); //recommended by someone from class. needed to clear my buffer to clean up my output
     setbuf(stdin, NULL);
-    struct Command command;
 
-    
+    //create a command object from header file to hold all the information about the users command
+    struct Command command; 
       
+    //print prompt and get input from user  
     printf(":");
     result = scanf("%2047[^\n]",line1);
     getchar();
@@ -106,14 +111,16 @@ int main(){
         
       }
     }
-    line[k] = '\0';
+    line[k] = '\0'; //set null terminator at end of our string
     k=0;
     i=0;
 
+    //if the user entered something then do all the logic for storing information about command line in our command object
     if(result > 0){
       command = get_command(line);
     }
 
+    /////////////////built in commands/////////////
     // if user enters exit, exit shell
     if(strcmp(command.array[0], "exit") == 0){
 
@@ -141,16 +148,20 @@ int main(){
         printf("exit code: %d\n", WIFEXITED(childStatus));
       }
     }
+
+    /////////////////end of built in commands/////////////
     
-    // Fork a new process
+    // if we are not running a built in command, fork a new process
     else{
-      pid_t spawnPid = fork();
+      pid_t spawnPid = fork(); //fork
 
       switch(spawnPid){
         case -1:
+          //if fork failed for some reason
           perror("fork()\n");
           exit(1);
           break;
+
         case 0:
           // In the child process
 
@@ -159,7 +170,9 @@ int main(){
             command.background = 0;
           }
 
+          //if we are in forground, then stop ignoring ctr+c
           if(command.background == 0){
+            //changing signal handler back to default for this child
             SIGINT_action.sa_handler = SIG_DFL; 
             SIGINT_action.sa_flags = SA_RESTART;
             sigfillset(&SIGINT_action.sa_mask);
@@ -180,24 +193,31 @@ int main(){
           break;
 
         default:
-          
           // In the parent process
-          
-          if(command.background == 1 && mode == 0){//if background and not in forground only mode, dont wait and add it to array
+
+          //if background and not in forground only mode, dont wait and add it to array of background PIDs
+          if(command.background == 1 && mode == 0){
+
+            //find empty spot in array
             while(processes[i] != 0){
               i++;
             }
-            printf("background pid is %d\n", spawnPid);
-            processes[i]=spawnPid;
+            printf("background pid is %d\n", spawnPid); //print background pid to console
+            processes[i]=spawnPid; //add PID to the found empty spot. Could add verification for if array is full, but I dont think we will have over 100 on OS1
             i=0;
           }
-          else{// Wait for child's termination
+          //if forground proccess, wait for child's termination
+          else{
             result = waitpid(spawnPid, &childStatus, 0);
+
+            //if it was interupted by a signal, print that signal to user
             if(!WIFEXITED(childStatus)){
               printf("\nterminated by signal %d\n", WTERMSIG(childStatus));
             }
           }
             
+          //every time we run loop, we want to check all the background PIDs and see if any are done
+          //if they are done, we need to wait on them and print they are done to the user before next prompt  
           while(i < 100){
             if(processes[i]!=0){
               result = waitpid(processes[i], &childStatus, WNOHANG);
@@ -214,9 +234,7 @@ int main(){
             i++; 
           }
           i=0; 
-          
-          
-          
+  
           break;
       }
     }
--- smallsh.h
+++ smallsh.h
@@ -2,20 +2,22 @@
 #include <math.h>
 
 struct Command{
-    char **array;
-    int flag;
-    int flag2; 
-    int background; 
-    int comment; 
-    int newOut, newIn;
-    int words; 
+    char **array; //to hold what user entered in prompt seperated by prompt
+    int flag; //flag for redirecting stdout (detect a >)
+    int flag2; //flag for redirection stdin (detect a <)
+    int background; //flag for if background (detect an '&' at the end of line)
+    int comment; //flag for if its a comment (detect #at biggining)
+    int newOut, newIn; //file descritors for new out and in files
+    int words; //word counter for how many arguments user entered
  
     char *output;//pointer to path for redirecting output
     char *input; //pointer to path for redirecting input
     
 };
-
+//make struct object based on information entered in prompt by user
 struct Command get_command(char line[2048]){
+
+    //make struct object and initialize default values
     struct Command command;
     command.background = 0;
     command.flag = 0;
@@ -23,6 +25,8 @@ struct Command get_command(char line[2048]){
     command.comment = 0;
     command.words = 1;
     
+    //walk through the user entered command line and detect special caracters and apply proper flags
+    //also keep track of number of words
     for(int i = 1; line[i]!='\0'; i++){
       if(line[i]=='>'){
         command.flag = i;
@@ -34,7 +38,7 @@ struct Command get_command(char line[2048]){
       }
       if(line[i] == '&' && line[i+1] == '\0'){
         command.background = 1;
-        command.words--;
+        command.words--;//subtract 1 from words to just remove the '&' at end
       }
 
       else if(line[i]==' ' || line[i]=='\n' || line[i]=='\t'){
@@ -42,125 +46,131 @@ struct Command get_command(char line[2048]){
       }
     }
 
-    //printf("\n%d, %d, %d", flag, flag2, flag3);
-    //allocate array to store correct number of words
-    //printf("\n%d, %d, %d\n", flag, flag2, flag3);
-    if(command.flag == 0 && command.flag2 == 0){
-      //allocate array to store correct number of words
-      command.array = malloc((command.words +1) * sizeof(char*));
-      //printf("hit");
 
+    ///////////////allocate array to store correct number of words///////////////////
+
+    //if not redirecting anything
+    if(command.flag == 0 && command.flag2 == 0){
+      //allocate size based on flags
+      command.array = malloc((command.words +1) * sizeof(char*));
+  
       //fill array
-      command.array[0] = strtok (line," &");
+      command.array[0] = strtok (line," ");
       for(int w = 1; w < command.words; w++){
-        command.array[w] = strtok (NULL," &");
+        command.array[w] = strtok (NULL," ");
       }
-      command.array[command.words] = NULL;
-      // for(int i = 0; i<command.words; i++){
-      //   printf("%s\n", command.array[i]);
-      // }
-      
+      command.array[command.words] = NULL; //set last array index to NULL for execvp()
     }
 
     //if only redirecting stdout
     else if(command.flag !=0 && command.flag2==0){
-      //printf("%d", words); 
+      //allocate size based on flags
       command.array = malloc((command.words - 1) * sizeof(char*));
+
+      //fill array
       command.array[0] = strtok (line," ");
-      //printf("token arg %d: %s\n", 0, array[0]);
       for(int w = 1; w < command.words - 2; w++){
         command.array[w] = strtok (NULL," ");
-        //printf("token arg %d: %s\n", w, array[w]);
       }
-      command.array[command.words-2] = NULL;
-      command.output = strtok (NULL," >");
-      //printf("output file: %s\n", output);
-      //fflush(stdout);
+      command.array[command.words-2] = NULL;//set last element to NULL for execv()
+      command.output = strtok (NULL," >"); //store output file name in output attribute of command object
     }
 
 
     //if only redirecting stdin
     else if(command.flag ==0 && command.flag2!=0){
+      //allocate size based on flags
       command.array = malloc((command.words - 1) * sizeof(char*));
+
+      //fill array
       command.array[0] = strtok (line," ");
       for(int w = 1; w < command.words - 2; w++){
         command.array[w] = strtok (NULL," ");
       }
-      command.array[command.words-2] = NULL;
-      command.input = strtok (NULL," <");
-      //printf("input: %s\n", input);
-      //fflush(stdout);
+      command.array[command.words-2] = NULL;//set last element to NULL for execv()
+      command.input = strtok (NULL," <");//store input file name in input attribute of command object
     }
 
     //if redirecting both stdin and stdout
     else if(command.flag !=0 && command.flag2!=0){
+      //allocate size based on flags
       command.array = malloc((command.words - (command.words - 2)) * sizeof(char*));
+
+      //fill array
       command.array[0] = strtok (line," ");
       for(int w = 1; w < (command.words - (command.words - 1)); w++){
         command.array[w] = strtok (NULL," ");
       }
-      command.array[command.words-(command.words - 1)] = NULL;
+      command.array[command.words-(command.words - 1)] = NULL;//set last element to NULL for execv()
+
+      //detect if < comes before > or vice versa
       if(command.flag < command.flag2){
-        command.output = strtok (NULL," <>");
-        command.input = strtok (NULL," <>");
+        command.output = strtok (NULL," <>");//store output file name in output attribute of command object
+        command.input = strtok (NULL," <>");//store input file name in input attribute of command object
 
       }
       else{
-        command.input = strtok (NULL," ><");
-        command.output = strtok (NULL," <>");
+        command.input = strtok (NULL," ><");///store input file name in input attribute of command object
+        command.output = strtok (NULL," <>");//store output file name in output attribute of command object
       }
-      //printf("arg1: %s\targ2: %s\n", array[0], array[1]);
-      //printf("input: %s\toutput: %s\n", input, output);
-      //fflush(stdout);
     }
     return command;
 }
 
 
 
-
-
-
 ////////////////////////////parnet logic ^^^^^^^^^^^^//////////////////
+
+
 /////////////////////////////child function vvvvv//////////////////////
 
 
 
-
-
+//child logic decides how we redirect things using the C api depending on flags
 void child_logic(struct Command command){
-  int newOut, newIn, result, dev_null;
-  // In the child process
-  //printf("CHILD(%d) running ls command\n", getpid());
+  int newOut, newIn, result, dev_null; //file desciptors
   
-  
-  //if its a comment
+  //if its a comment just exit (detects if user entered # at the biginning)
+  //I guess this is kind of redundant because im not useing my "comment" flag in the struct.
   if(command.array[0][0]=='#'){
     exit(0);
   }
 
-  //if its a non specifide io background task
+  //if its a non specifide io background task redirect stdin and stdout to /dev/null
   else if(command.flag == 0 && command.flag2 == 0 && command.background ==1){
+    //create file discriptor for /dev/null for stdin (read only)
     dev_null = open("/dev/null", O_RDONLY);
+
+    //if couldnt open, error and exit
     if(dev_null == -1){
       perror("dev_null fopen()");
       exit(1);
     }
+
     //set stdin in to /dev/null
     result = dup2(dev_null, 0);
+
+    //if cant redirect std in, error and exit
     if(result == -1){
-      perror("> /dev/null dub2()");
+      perror("/dev/null dub2()");
       exit(2);
     }
+
+    //create file discriptor for /dev/null for stdout (write only)
     dev_null = open("/dev/null", O_WRONLY);
+
+    //if couldnt open, error and exit
     if(dev_null == -1){
       perror("dev_null fopen()");
       exit(1);
     }
+
     //set stdout in to /dev/null
     result = dup2(dev_null, 1);
+
+    //if cant redirect std out, error and exit
     if(result == -1){
-      perror("< /dev/null dub2()");
+      perror("/dev/null dub2()");
       exit(2);
     }
 
@@ -168,17 +178,21 @@ void child_logic(struct Command command){
 
   //if only redirecting stdout
   else if(command.flag !=0 && command.flag2==0){ 
+    //open file for new out for write only, creat if dont exits, and truncate if already exists. 
     newOut = open (command.output, O_WRONLY | O_CREAT | O_TRUNC, 0777);
     if(newOut == -1){
       perror("newOUt fopen()");
       exit(1);
     }
+    
+    //redirect stdout to new output 
     result = dup2(newOut, 1);
     if(result == -1){
       perror("newOUt dub2()");
       exit(2);
     }
-    //if background command with unspecifid stdin
+
+    //if background command with unspecifid stdin, redirect stdin to /dev/null
     if(command.background == 1){
       dev_null = open("/dev/null", O_RDONLY);
       if(dev_null == -1){
@@ -196,17 +210,21 @@ void child_logic(struct Command command){
 
   //if only redirecting stdin
   else if(command.flag ==0 && command.flag2!=0){
+    //open file for read only for new stdin
     newIn = open(command.input, O_RDONLY);
     if(newIn == -1){
       perror("newIn open()");
       exit(1);
     }
+
+    //redirect stdin to new file
     result = dup2(newIn, 0);
     if(result == -1){
       perror("newIn dub2()");
       exit(2);
     }
-    //if background command with unspecifid stdout
+
+    //if background command with unspecifid stdout, set stdout to /dev/null
     if(command.background == 1){
       dev_null = open("/dev/null", O_WRONLY);
       if(dev_null == -1){
@@ -226,25 +244,28 @@ void child_logic(struct Command command){
 
   //if redirecting both stdin and stdout
   else if(command.flag !=0 && command.flag2!=0){
-
-    //redirecting stdout
+    //open new file for new std out. Open for read only, create if dont exist, and truncate if already exists.
     newOut = open (command.output, O_WRONLY | O_CREAT | O_TRUNC, 0777);
     if(newOut == -1){
       perror("newOUt fopen()");
       exit(1);
     }
+
+    //redirect std out to new file discriptor.
     result = dup2(newOut, 1);
     if(result == -1){
       perror("newOUt dub2()");
       exit(2);
     }
 
-    //redirecting stdin
+    //open file for read only for new stdin
     newIn = open(command.input, O_RDONLY);
       if(newIn == -1){
       perror("newIn open()");
       exit(1);
     }
+
+    //redirecting stdin to new file discriptor.
     result = dup2(newIn, 0);
       if(result == -1){
       perror("newIn dub2()");
